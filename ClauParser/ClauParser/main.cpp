813e3e3ec322eed137d617923c6383842d00babe
DongWook Lee <vztpv@naver.com>
2020-06-08 06:08:23 +0900

    Add files via upload

--- ClauParser/ClauParser/main.cpp
+++ ClauParser/ClauParser/main.cpp
@@ -1,11 +1,69 @@
 
+#define _CRT_SECURE_NO_WARNINGS
+//#include <vld.h>
+
 #include <iostream>
+
 #include "clau_parser.h"
 
+#include <ctime>
 
-int main(void) 
+
+int main(void)
 {
+	/*
+	{
+		wiz::UserType ut("Name");
+		wiz::ItemType<std::string> it("Name", "Data");
+		
+		ut.AddItemType(it);
+	}
 
+
+	{
+		wiz::UserType ut("Name");
+		wiz::UserType global("Name");
+
+		global.AddUserTypeItem(ut); // 내부에서 동적할당
+	}
+
+
+
+
+	{
+		wiz::UserType global;
+		global.AddItem("date", "1947.5.8");
+		global.AddItem("save_game", "\"Russia.eu4\"");
+		{
+			wiz::UserType temp("savegame_versions");
+			temp.AddItem("", "\"1.18.1.0\"");
+			temp.AddItem("", "\"1.18.2.0\"");
+			temp.AddItem("", "\"1.18.3.0\"");
+			temp.AddItem("", "\"1.18.4.0\"");
+			temp.AddItem("", "\"1.19.1.0\"");
+			global.AddUserTypeItem(temp);
+		}
+
+		std::cout << global.ToString();
+	}
+	*/
+
+	wiz::UserType global;
+	std::string fileName = "input.eu4"; 
+	
+	std::cin >> fileName;
+	
+
+	int a = clock();
+
+	wiz::LoadData::LoadDataFromFile(fileName, global, 0, 0);
+
+	int b = clock();
+
+	std::cout << b - a << "ms" << "\n";
+	
+	wiz::LoadData::SaveWizDB2(global, "output.eu4");
+	
 
 	return 0;
 }
